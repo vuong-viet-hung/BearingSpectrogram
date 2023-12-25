decd34d0c4d6e8980b6239f8d3d70c3626404cf0
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-25 21:50:08 +0700

    Save cfs in segment

--- bearingSpectrogram/cwru/saveCwruCfs.m
+++ bearingSpectrogram/cwru/saveCwruCfs.m
@@ -11,19 +11,22 @@ function saveCwruCfs(signalDir, cfsDir)
     faultFiles = listDir(fullfile(signalDir, "12k_DE/*.mat"));
     signalFiles = vertcat([normalFiles, faultFiles]);
     samplingFrequency = 12000;
+    rpms = [1797, 1772, 1750, 1730];
     
     for i = 1:numel(signalFiles)
         signal = loadCwruSignal(signalFiles(i));
         [~, fileName, ~] = fileparts(signalFiles(i));
-        fileName = fileName + ".mat";
         [trainSplit, valSplit] = splitSignal(signal, 0.2);
-        disp("Preprocessing: " + fileName);
+        fprintf("Processing: %s.mat\n", fileName);
         signalSplits = {signal, trainSplit, valSplit};
         for j = 1:numel(splits)
             imfSum = sumImf(signalSplits{j}, 3);
-            cfs = createCfs(imfSum, samplingFrequency);
-            cfsFile = fullfile(cfsDir, splits(j), fileName);
-            save(cfsFile, "cfs");
+            subStrs = split(fileName, "_");
+            hp = str2double(subStrs(2));
+            rpm = rpms(hp + 1);
+            segmentLength = computeSegmentLength(samplingFrequency, rpm);
+            saveDir = fullfile(cfsDir, splits(j), fileName);
+            saveCfs(imfSum, segmentLength, samplingFrequency, saveDir);
         end
     end
 end
