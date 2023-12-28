3007eb1e7dc10997fee1f3a85cef3417c8fad89d
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-28 13:42:55 +0700

    Minor refactors

--- bearingSpectrogram/cwru/saveCwruStft.m
+++ bearingSpectrogram/cwru/saveCwruStft.m
@@ -1,6 +1,6 @@
 function saveCwruStft(signalDir, stftDir)
-    normalFiles = listDir(fullfile(signalDir, "Normal/*.mat"));
-    faultFiles = listDir(fullfile(signalDir, "12k_DE/*.mat"));
+    normalFiles = listDir(fullfile(signalDir, "Normal", "*.mat"));
+    faultFiles = listDir(fullfile(signalDir, "12k_DE", "*.mat"));
     signalFiles = vertcat([normalFiles, faultFiles]);
     samplingFrequency = 12000;
     rpms = [1797, 1772, 1750, 1730];
@@ -9,8 +9,8 @@ function saveCwruStft(signalDir, stftDir)
         signal = loadCwruSignal(signalFiles(i));
         [~, fileName, ~] = fileparts(signalFiles(i));
         fprintf("Processing: %s.mat\n", fileName);
-        subStrs = split(fileName, "_");
-        hp = str2double(subStrs(2));
+        subStr = split(fileName, "_");
+        hp = str2double(subStr(2));
         rpm = rpms(hp + 1);
         segmentLength = ceilDiv(samplingFrequency * 60, rpm);
         saveDir = fullfile(stftDir, fileName);
