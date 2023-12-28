a9436129e40c1279a8fceac1ba5f35f25ea5d0d3
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-28 19:52:34 +0700

    Extract computation of shaft frequency to a function

--- bearingSpectrogram/cwru/saveCwruSpectrograms.m
+++ bearingSpectrogram/cwru/saveCwruSpectrograms.m
@@ -2,19 +2,27 @@ function saveCwruSpectrograms(signalDir, spectrogramDir)
     normalFiles = listDir(fullfile(signalDir, "Normal", "*.mat"));
     faultFiles = listDir(fullfile(signalDir, "12k_DE", "*.mat"));
     signalFiles = vertcat([normalFiles, faultFiles]);
-    samplingFrequency = 12000;
-    rpms = [1797, 1772, 1750, 1730];
+    samplingFreq = 12000;
     
     for i = 1:length(signalFiles)
-        signal = loadCwruSignal(signalFiles(i));
         [~, fileName, ~] = fileparts(signalFiles(i));
         fprintf("Processing: %s.mat\n", fileName);
-        % To extract hp from file name
-        subStr = split(fileName, "_");
-        hp = str2double(subStr(2));
-        rpm = rpms(hp + 1);
-        segmentLength = ceilDiv(samplingFrequency * 60, rpm);
+
+        signal = loadCwruSignal(signalFiles(i));
+        shaftFreq = shaftFreqFrom(fileName);
+        segmentLength = ceilDiv(samplingFreq, shaftFreq);
         saveDir = fullfile(spectrogramDir, fileName);
         saveSpectrograms(signal, segmentLength, saveDir);
     end
+end
+
+
+function shaftFreq = shaftFreqFrom(fileName)
+    hps = ["0", "1", "2", "3"];
+    rpms = [1797, 1772, 1750, 1730];
+    hpToRpm = containers.Map(hps, rpms);
+    subStrs = split(fileName, "_");
+    hp = subStrs(2);
+    rpm = hpToRpm(hp);
+    shaftFreq = rpm / 60;
 end
