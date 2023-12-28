5110eff94471cc924999101c04429c62112fbc6f
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-28 19:50:31 +0700

    Keep negative instead of positive frequency half

--- bearingSpectrogram/core/saveSpectrograms.m
+++ bearingSpectrogram/core/saveSpectrograms.m
@@ -7,9 +7,12 @@ function saveSpectrograms(signal, segmentLength, saveDir)
     for i = 1:size(segments, 1)
         segment = segments(i, :);
         spec = abs(cqt(segment));
-        % To keep only the positive frequency
-        spec = spec(floorDiv(size(spec, 1), 2) + 1:end, :);
+        % Keep only the negative frequency half
+        spec = spec(1:floorDiv(size(spec, 1), 2), :);
         file = fullfile(saveDir, sprintf("%04d.mat", i - 1));
         save(file, "spec");
     end
 end
+
+
+
