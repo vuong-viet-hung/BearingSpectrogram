d12d6f8ddeedadd52fc4f0f1939c98e08e14485b
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-02 01:10:30 +0700

    Update project

--- bearingSpectrogram/common/saveSpectrograms.m
+++ bearingSpectrogram/common/saveSpectrograms.m
@@ -2,12 +2,12 @@ function saveSpectrograms(signal, segmentLength, saveDir)
     if ~isfolder(saveDir)
         mkdir(saveDir);
     end
-
+    
     segments = segmentSignal(signal, segmentLength);
-    for i = 1:size(segments, 1)
-        segment = segments(i, :);
+    for i = 1:size(segments, 2)
+        segment = segments(:, i);
         spec = computeSpectrogram(segment);
-        file = fullfile(saveDir, sprintf("%04d.mat", i - 1));
+        file = fullfile(saveDir, sprintf("%04d.png", i - 1));
         % Save spectrogram as it's computed on one segment for memory
         % suffieciency
         save(file, "spec");
@@ -18,13 +18,16 @@ end
 function segments = segmentSignal(signal, segmentLength)
     remainder = mod(length(signal), segmentLength);
     numSegments = floorDiv(length(signal), segmentLength);
-    segments = reshape(signal(1:end - remainder), numSegments, ...
-        segmentLength);
+    segments = reshape(signal(1:end - remainder), segmentLength, ...
+        numSegments);
 end
 
 
 function spec = computeSpectrogram(segment)
-    spec = abs(cqt(segment));
+    spec = 10 * log10(abs(cqt(segment)));
+    specMean = mean(spec, "all");
+    specStd = std(spec, 1, "all");
+    spec = (spec - specMean) / specStd;
     % Keep only the negative frequency half
     spec = spec(1:floorDiv(size(spec, 1), 2), :);
 end
